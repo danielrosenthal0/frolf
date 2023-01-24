c89806f7e555abe3433954f48b2bf97379836423
Jack Connelly <connjm19@wfu.edu>
2023-01-24 16:10:12 -0500

    changes

--- image_tracking.py
+++ image_tracking.py
@@ -24,6 +24,11 @@ cap = cv2.VideoCapture('/Users/jackconnelly/Documents/School/Fall_2022/EGR_314/O
 lower_color = np.array([0, 50, 170])
 upper_color = np.array([255, 255, 255])
 
+# ######################################################################################################################################
+# black rgb lower upper
+lower_colorb = np.array([0, 0, 0])
+upper_colorb = np.array([255, 255, 100])
+# ######################################################################################################################################
 maxLength = 0
 loc = ""
 centerX = []
@@ -31,24 +36,25 @@ centerY = []
 area = []
 while True:
     
-    # Capture the current frame
+    # Capture the current frameq
     ret, frame = cap.read()
+    frame2 = frame
     if not ret:
         break
     # Convert the frame to the hsv color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # rgb color space for second mask
+    rgb = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
+
     # Threshold the frame to get only the colors in the specified range
     mask = cv2.inRange(hsv, lower_color, upper_color)
-
     # Use morphological transformations to reduce noise
     kernel = np.ones((5, 5), np.uint8)
     mask = cv2.erode(mask, kernel, iterations=1)
     mask = cv2.dilate(mask, kernel, iterations=1)
-
     # Find the contours in the mask
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     x = np.array([])
     y = np.array([])
 
@@ -64,7 +70,6 @@ while True:
             # saving all locations of center of contours for calculations 
             centerX.append(cX)
             centerY.append(cY)
-
         else:
             cX, cY = 0, 0
         # Draw the contour and the center of the contour on the frame
@@ -95,7 +100,7 @@ while True:
     try:
         x = np.subtract(x, np.amin(x) - 1)
         y = np.subtract(y, np.amin(y) - 1)
-        xmax = int(np.amax(x) + 1)
+        xmax = int(np.amax(x) + 1)  
         ymax = int(np.amax(y) + 1)
         loc = np.zeros((xmax, ymax), dtype=np.int16)
         i = 0
@@ -104,13 +109,36 @@ while True:
             i = i + 1
     except:
         print("woops")
+
+
+    mask2  = cv2.inRange(rgb, lower_colorb, upper_colorb)
+    mask2 = cv2.erode(mask2, kernel, iterations=1)
+    mask2 = cv2.dilate(mask2, kernel, iterations=1)
+    contours2, h = cv2.findContours(mask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if len(contours2) > 0:
+        c = max(contours2, key=cv2.contourArea)
+        # Find the center of the contour
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        else:
+            cX, cY = 0, 0
+        # Draw the contour and the center of the contour on the frame
+        cv2.drawContours(frame, [c], -1, (255, 0, 0), 2)
+        cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(frame, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    
+
     # Show the frame
     cv2.imshow("Frame", frame)
     cv2.imshow("mask", mask)
+    cv2.imshow("mask2", mask2)
     # time.sleep(.3)
     # Exit the loop if the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
 # Release the video capture object and close all windows
 cap.release()
 cv2.destroyAllWindows()
@@ -138,7 +166,7 @@ for poop in centerX:
     thisVal = np.array((centerX[i], centerY[i]))
     if len(lastVal) == 2:
         inV.append(abs(np.linalg.norm(thisVal - lastVal)))
-        horz.append(np.arctan((thisVal[1] - lastVal[1])/(thisVal[0]-lastVal[0])))
+        horz.append(np.arctan((thisVal[1]-lastVal[1])/(thisVal[0]-lastVal[0])))
     lastVal = thisVal
     i = i + 1
 horz = np.degrees(horz)
